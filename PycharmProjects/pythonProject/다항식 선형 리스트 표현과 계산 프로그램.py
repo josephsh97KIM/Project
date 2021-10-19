a26ff53fe37abf02d1481e0985e6a07885e74f4f
SukHyun Kim <84235048+josephsh97KIM@users.noreply.github.com>
2021-10-19 22:25:55 +0900

    Add files via upload

---
+++ PycharmProjects/pythonProject/다항식 선형 리스트 표현과 계산 프로그램.py
@@ -0,0 +1,42 @@
+##함수 선언 부분##
+def printPoly(p_x):
+    term = len(p_x) - 1      #최고차항 숫자 = 배열 길이 - 1
+    polyStr = "P(x)= "
+
+    for i in range(len(px)) :
+        coef = p_x[i]         #계수
+
+        if coef ==0:  #계수가 0이면 생략
+            term -= 1
+            continue
+        if (coef>=0 and i !=0 ) : # 최고차항 앞의 +는 출력하지 않도록 함
+            polyStr += "+"
+        polyStr += str(coef) + "x^" + str(term) + " "
+        term -= 1
+
+    return polyStr
+
+def calcPoly(xVal, p_x):
+    retValue = 0
+    term = len(p_x) - 1
+
+    for i in range(len(px)):
+        coef = p_x[i]
+        retValue += coef * xValue ** term
+        term -= 1                         #= 7x^3 -4x^2 +0x^1 +5x^0
+
+    return retValue
+
+##전역 변수 선언 부분##
+px= [7, -4, 0, 5]
+
+##메인코드 부분##
+if __name__ == "__main__":
+
+    pStr = printPoly(px)
+    print(pStr)
+
+    xValue = int (input("x값-->"))
+
+    pxValue = calcPoly(xValue,px)
+    print(pxValue)
